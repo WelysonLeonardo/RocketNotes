297ce5279b53d810d2925ecf46a6b6f88e90ae2e
Welyson <welyson.leonardo@casadosaber.com.br>
2024-08-13 08:21:45 -0300

    created error mensage

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -20,6 +20,16 @@ export function SignUp() {
         }
 
         api.post("/users", { name, email, password })
+        .then(() => {
+            alert("Usuário cadastrado com sucesso!");
+        })
+        .catch(error => {
+            if(error.response){
+                alert(error.response.data.message);
+            } else {
+                alert ("Não foi possível cadastrar sua conta");
+            }
+        })
     }
 
     return (
