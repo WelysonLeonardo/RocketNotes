b84f4480ec8f7c826d7518dd0a58517ec9fb789a
Welyson <welyson.leonardo@casadosaber.com.br>
2024-08-13 21:40:42 -0300

    update auth hook

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,11 +1,11 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-    const [data, setData] = useState(" ");
+    const [data, setData] = useState({});
 
     async function signIn({email, password}){
 
@@ -13,17 +13,31 @@ function AuthProvider({ children }) {
             const response = await api.post("/sessions", { email, password});
             const { user, token } = response.data;
 
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+            localStorage.setItem("@rocketnotes:token", token);
+
             api.defaults.headers.authorization = `Bearer ${token}`;
             setData({ user, token });
 
         } catch(error){
             if(error.response){
-                alert(error.response.data.menssage);
+                alert(error.response.data.message);
             } else {
                 alert("Ocorreu um erro ao tentar fazer login.");
             }
         }
     }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketnotes:token");
+        const user = localStorage.getItem("@rocketnotes:user");
+
+        if(token && user){
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            setData({ user: JSON.parse(user), token });
+        }
+
+    }, []);
 
     return (
         <AuthContext.Provider value={{ signIn, user: data.user }}> 
