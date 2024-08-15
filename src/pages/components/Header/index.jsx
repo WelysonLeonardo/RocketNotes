f7feddef5173c698654c80916342d066f84902ac
Welyson <welyson.leonardo@casadosaber.com.br>
2024-08-15 18:50:25 -0300

    update auth routes

--- src/pages/components/Header/index.jsx
+++ src/pages/components/Header/index.jsx
@@ -3,11 +3,18 @@ import { useAuth } from '../../../hooks/auth';
 
 import { api } from '../../../services/api';
 import avatarPlaceholder from "../../../assets/avatar_placeholder.svg";
+import { useNavigate } from 'react-router-dom';
 
 import { Container, Profile, Logout } from './styles'
 
 export function Header() {
   const { signOut, user } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSignOut(){
+    navigate('/');
+    signOut();
+  }
 
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
@@ -25,7 +32,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
 
